e4d05e5369ab12fe29216b591bf520b20d58fc47
karina112We <162794922+karina112We@users.noreply.github.com>
2024-03-16 12:57:30 +0500

    Add files via upload

--- second_win.py
+++ second_win.py
@@ -1,1 +1,2 @@
-# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit)
